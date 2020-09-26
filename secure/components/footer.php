7cf473727e6b1a6f9ec1b3f2f590a4a55fe3f053
Skywrath <59442276+blackxhat24@users.noreply.github.com>
2020-09-26 14:49:41 +0700

    Update footer.php

--- secure/components/footer.php
+++ secure/components/footer.php
@@ -1,13 +1,12 @@
 <footer>
     <div class="container">
         <div class="col-md-12">
-            Luora
+            Freak
         </div>
         <div class="col-sm-6 col-lg-6">
             <a href="article.php" style="color: #b92b27;">Article</a>
         </div>
         <div class="col-sm-6 col-md-6 col-lg-6" style="text-align: right;">
-            Software Laboratory Center Binus University
         </div>
     </div>
 </footer>
