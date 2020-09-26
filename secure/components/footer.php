ab537d283c02d56ff1f91118a0c3ffe5238a0873
Skywrath <59442276+blackxhat24@users.noreply.github.com>
2020-09-26 14:49:07 +0700

    Add files via upload

---
+++ secure/components/footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="container">
+        <div class="col-md-12">
+            Luora
+        </div>
+        <div class="col-sm-6 col-lg-6">
+            <a href="article.php" style="color: #b92b27;">Article</a>
+        </div>
+        <div class="col-sm-6 col-md-6 col-lg-6" style="text-align: right;">
+            Software Laboratory Center Binus University
+        </div>
+    </div>
+</footer>
